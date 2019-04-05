64620f98641846f2376a7e7b8b19059151eb1b54
Ed <hayrapetyan-edo@inbox.ru>
2019-04-05 14:21:27 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+dotenv.config({
+    path: './.env'
+})
+
+let transporter = nodemailer.createTransport({
+  service: 'mail.ru',
+  secure: false,
+  port: 25,
+  auth: {
+    user : process.env.EMAIL,
+    pass:  process.env.SECRET_MESSAGE
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
+
+let HelperOptions = {
+  from: '"NAME" <YOUREMAIL',
+  to: process.env.EMAIL,
+  subject: 'awesome subject',
+  text: 'Awesome MSG'
+};
+
+
+
+  transporter.sendMail(HelperOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("The message was sent!");
+    //console.log(info);
+  });
+
+
+ 
